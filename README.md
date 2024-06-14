7a805653b42121088e9a658b16fef6966f908a67
Caleb Leinz <cleinz+github@pm.me>
2024-06-14 16:44:27 -0700

    docs: Improve public docs

--- README.md
+++ README.md
@@ -6,6 +6,56 @@ This project is similar in scope to [stubborn-io](https://github.com/craftytrick
 but aims to leverage the recently stabilized `async fn` in traits, to make the
 implementation of reconnecting simpler for the end user.
 
+## Usage
+
+In most cases, it is expected that the consumer of this library will want to
+implement their own `TetherResolver` types. This allows them to inject arbitrary
+asynchronous code just before the I/O attempts to reconnect.
+
+```rust
+use tokio::{net::TcpStream, io::AsyncReadExt, sync::mpsc};
+
+/// Custom resolver
+pub struct CallbackResolver {
+	channel: mpsc::Sender<String>,
+}
+
+impl TetherResolver for CallbackResolver {
+	type Error = std::io::Error;
+
+    async fn disconnected(
+        &mut self,
+        context: &Context,
+        state: &State<Self::Error>,
+    ) -> bool {
+		match state {
+			State::Eof => false,
+			State::Err(error) => {
+				let error = error.to_string();
+				self.channel.send(error).await.unwrap();
+				true	
+			}
+		}
+	}
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let mut buf = Vec::new();
+	let (channel, rx) = mpsc::channel(10);
+
+	let resolver = CallbackResolver {
+		channel,
+	};
+	let mut tether = Tether::<TcpStream>::connect("localhost:8080", resolver)
+		.await?;
+
+	tether.read_buf(&mut buf).await?;
+	Ok(())
+}
+
+```
+
 ## State
 
 This project is still very much a work in progress, expect fairly common 
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,35 @@ mod implementations;
 ///
 /// Since the disconnected method asynchronous, and is invoked when the underlying stream
 /// disconnects things like `tokio::time::sleep` work out of the box.
+// TODO: Remove the Unpin restriction
 pub trait TetherResolver: Unpin {
     type Error;
 
+    /// Invoked by Tether when an error/disconnect is encountered.
+    ///
+    /// Returning `true` will result in a reconnect being attempted via `<T as TetherIo>::reconnect`,
+    /// returning `false` will result in the error being returned from the originating call.
+    ///
+    /// # Note
+    ///
+    /// The [`State`] will describe the type of the underlying error. It can either be `State::Eof`,
+    /// in which case the end of file was reached, or an error. This information can be leveraged
+    /// in this function to determine whether to attempt to reconnect.
     fn disconnected(
         &mut self,
         context: &Context,
         state: &State<Self::Error>,
     ) -> impl Future<Output = bool> + Send;
 
+    /// Invoked by Tether when an end of file (EOF) is detected.
+    ///
+    /// If the implementer returns `true` here, `disconnect` will be invoked with `&State::Eof`.
+    /// Returning `false` will allow the Tether to terminate.
+    ///
+    /// # Note
+    ///
+    /// This function is called each time an EOF is detected, allowing the implementer to decide
+    /// dynamically whether to attempt to reconnect.
     fn eof_triggers_reconnect(&mut self) -> bool;
 }
 
@@ -26,7 +46,13 @@ pub trait TetherResolver: Unpin {
 pub trait TetherIo<T>: Sized + Unpin {
     type Error;
 
+    /// Initializes the connection to the I/O source
     fn connect(initializer: &T) -> impl Future<Output = Result<Self, Self::Error>> + Send;
+
+    /// Re-establishes the connection to the I/O source
+    fn reconnect(initializer: &T) -> impl Future<Output = Result<Self, Self::Error>> + Send {
+        Self::connect(initializer)
+    }
 }
 
 enum Status<E> {
@@ -51,6 +77,20 @@ impl Into<std::io::Error> for State<std::io::Error> {
 }
 
 /// A wrapper type which contains the underying I/O object, it's initializer, and resolver.
+///
+/// This in the main type exposed by the library. It implements [`AsyncRead`](tokio::io::AsyncRead)
+/// and [`AsyncWrite`](tokio::io::AsyncWrite) whenever the underying I/O object implements them.
+///
+/// Calling things like `read_buf` will result in the I/O automatically reconnecting if an error
+/// is detected during the underlying I/O call.
+///
+/// # Note
+///
+/// Currently, there is no way to obtain a reference into the underlying I/O object. And the only
+/// way to reclaim the inner I/O type is by calling [`Tether::into_inner`]. This is by design, since
+/// in the future there may be reason to add unsafe code which cannot be guaranteed if outside
+/// callers can obtain references. In the future I may add these as unsafe functions if those
+/// cases can be described.
 pub struct Tether<I, T, R> {
     context: Context,
     initializer: I,
@@ -59,12 +99,24 @@ pub struct Tether<I, T, R> {
 }
 
 impl<I, T, R> Tether<I, T, R> {
+    /// Returns a reference to the resolver
     pub fn get_resolver(&self) -> &R {
         &self.resolver
     }
 
+    /// Returns a mutable reference to the resolver
     pub fn get_resolver_mut(&mut self) -> &mut R {
         &mut self.resolver
+    }
+
+    /// Returns a reference to the initializer
+    pub fn get_initializer(&self) -> &I {
+        &self.initializer
+    }
+
+    /// Returns a mutable reference to the initializer
+    pub fn get_initializer_mut(&mut self) -> &mut I {
+        &mut self.initializer
     }
 
     /// Consume the Tether, and return the underlying I/O type
@@ -72,18 +124,12 @@ impl<I, T, R> Tether<I, T, R> {
         self.inner
     }
 
-    pub fn get_inner(&self) -> &T {
-        &self.inner
-    }
-
-    pub fn get_inner_mut(&mut self) -> &mut T {
-        &mut self.inner
-    }
-
+    /// Returns a reference to the context
     pub fn get_context(&self) -> &Context {
         &self.context
     }
 
+    /// Returns a mutable reference to the context
     pub fn get_context_mut(&mut self) -> &mut Context {
         &mut self.context
     }
@@ -93,7 +139,11 @@ impl<I, T, R> Tether<I, T, R>
 where
     T: TetherIo<I>,
 {
-    pub async fn new(initializer: I, resolver: R) -> Result<Self, T::Error> {
+    /// Connect to the I/O source
+    ///
+    /// Invokes TetherIo::connect to establish the connection, the same method which is called
+    /// when Tether attempts to reconnect.
+    pub async fn connect(initializer: I, resolver: R) -> Result<Self, T::Error> {
         let inner = T::connect(&initializer).await?;
 
         let me = Self {
@@ -119,6 +169,9 @@ pub struct Context {
 }
 
 impl Context {
+    /// The number of times a reconnect has been attempted.
+    ///
+    /// The first time [`TetherResolver::disconnected`] is invoked this will return `1`.
     pub fn reconnect_count(&self) -> usize {
         self.reconnection_attempts
     }
@@ -149,7 +202,7 @@ where
                 return Poll::Ready(Status::Failover(state));
             }
 
-            let fut = T::connect(&self.initializer);
+            let fut = T::reconnect(&self.initializer);
             let fut_pin = std::pin::pin!(fut);
             match ready::ready!(fut_pin.poll(cx)) {
                 Ok(new_stream) => {
