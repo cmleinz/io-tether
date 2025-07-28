f7c9be63950b12ded9dd38193e28938dfc7578c0
Caleb Leinz <cleinz+github@pm.me>
2025-07-28 11:11:59 -0700

    feat: Add futures::Stream implementation

--- src/implementations.rs
+++ src/implementations.rs
@@ -2,9 +2,12 @@ use std::{ops::ControlFlow, pin::Pin, task::Poll};
 
 use tokio::io::{AsyncRead, AsyncWrite};
 
-use crate::{Reason, Source, State, TetherInner, config::ErrorPropagation};
+use crate::{Reason, Source, State, TetherInner};
 
 use super::{Io, Resolver, Tether, ready::ready};
+
+#[cfg(feature = "stream")]
+mod stream;
 
 /// I want to avoid implementing From<Reason> for Result<T, std::io::Error>, because it's not a
 /// generally applicable transformation. In the specific case of AsyncRead and AsyncWrite, we can
@@ -32,57 +35,63 @@ impl IoInto<Result<(), std::io::Error>> for Reason {
     }
 }
 
-macro_rules! connected {
-    ($me:expr, $poll_method:ident, $cx:expr, $default:expr, $($args:expr),*) => {
-        loop {
-            match $me.state {
-                State::Connected => {
-                    let cont = ready!($me.inner.$poll_method(&mut $me.state, $cx, $($args),*));
+pub(crate) mod connected {
+    macro_rules! connected {
+        ($me:expr, $poll_method:ident, $cx:expr, $default:expr, $($args:expr),*) => {
+            loop {
+                match $me.state {
+                    State::Connected => {
+                        let cont = ready!($me.inner.$poll_method(&mut $me.state, $cx, $($args),*));
 
-                    if let ControlFlow::Break(val) = cont {
-                        return Poll::Ready(val);
-                    }
-                }
-                State::Disconnected(ref mut fut) => {
-                    let retry = ready!(fut.as_mut().poll($cx));
-
-                    if retry {
-                        $me.inner.set_reconnecting(&mut $me.state);
-                        continue;
-                    }
-
-                    let opt_reason = $me.inner.context.reason.take();
-                    let reason = opt_reason.expect("Can only enter Disconnected state with Reason");
-
-                    let reason = match ($me.inner.config.error_propagation_on_no_retry, reason.1) {
-                        (ErrorPropagation::IoOperations, Source::Io) => reason.0.io_into(),
-                        (ErrorPropagation::All, _) => reason.0.io_into(),
-                        _ => $default,
-                    };
-
-                    return Poll::Ready(reason);
-                }
-                State::Reconnecting(ref mut fut) => {
-                    let result = ready!(fut.as_mut().poll($cx));
-                    $me.inner.context.increment_attempts();
-
-                    match result {
-                        Ok(new_io) => {
-                            $me.inner.set_reconnected(&mut $me.state, new_io);
+                        if let ControlFlow::Break(val) = cont {
+                            return Poll::Ready(val);
                         }
-                        Err(error) => {
-                            $me.inner.set_disconnected(&mut $me.state, Reason::Err(error), Source::Reconnect);
-                        },
                     }
-                }
-                State::Reconnected(ref mut fut) => {
-                    ready!(fut.as_mut().poll($cx));
-                    $me.inner.set_connected(&mut $me.state);
+                    State::Disconnected(ref mut fut) => {
+                        let retry = ready!(fut.as_mut().poll($cx));
+
+                        if retry {
+                            $me.inner.set_reconnecting(&mut $me.state);
+                            continue;
+                        }
+
+                        let opt_reason = $me.inner.context.reason.take();
+                        let reason = opt_reason.expect("Can only enter Disconnected state with Reason");
+
+                        let reason = match ($me.inner.config.error_propagation_on_no_retry, reason.1) {
+                            (crate::config::ErrorPropagation::IoOperations, Source::Io) => reason.0.io_into(),
+                            (crate::config::ErrorPropagation::All, _) => reason.0.io_into(),
+                            _ => $default,
+                        };
+
+                        return Poll::Ready(reason);
+                    }
+                    State::Reconnecting(ref mut fut) => {
+                        let result = ready!(fut.as_mut().poll($cx));
+                        $me.inner.context.increment_attempts();
+
+                        match result {
+                            Ok(new_io) => {
+                                $me.inner.set_reconnected(&mut $me.state, new_io);
+                            }
+                            Err(error) => {
+                                $me.inner.set_disconnected(&mut $me.state, Reason::Err(error), Source::Reconnect);
+                            },
+                        }
+                    }
+                    State::Reconnected(ref mut fut) => {
+                        ready!(fut.as_mut().poll($cx));
+                        $me.inner.set_connected(&mut $me.state);
+                    }
                 }
             }
-        }
-    };
+        };
+    }
+
+    pub(crate) use connected;
 }
+
+use connected::connected;
 
 impl<C, R> TetherInner<C, R>
 where
---
+++ src/implementations/stream.rs
@@ -0,0 +1,68 @@
+use std::{ops::ControlFlow, task::Poll};
+
+use futures_core::Stream;
+
+use crate::{
+    Io, Reason, Resolver, Source, State, Tether, TetherInner,
+    implementations::{IoInto, connected::connected},
+    ready::ready,
+};
+
+impl<T> IoInto<Option<Result<T, std::io::Error>>> for Reason {
+    fn io_into(self) -> Option<Result<T, std::io::Error>> {
+        match self {
+            Reason::Eof => None,
+            Reason::Err(error) => Some(Err(error)),
+        }
+    }
+}
+
+impl<C, R, T, E> TetherInner<C, R>
+where
+    C: Io + Unpin,
+    C::Output: Stream<Item = Result<T, E>> + Unpin,
+    R: Resolver<C> + Unpin,
+    E: Into<std::io::Error>,
+{
+    fn poll_stream_inner(
+        &mut self,
+        state: &mut State<C::Output>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<ControlFlow<Option<std::io::Result<T>>>> {
+        let item = {
+            let inner_pin = std::pin::pin!(&mut self.io);
+            ready!(inner_pin.poll_next(cx))
+        };
+
+        match item {
+            None => {
+                self.set_disconnected(state, Reason::Eof, Source::Io);
+                Poll::Ready(ControlFlow::Continue(()))
+            }
+            Some(Ok(value)) => Poll::Ready(ControlFlow::Break(Some(Ok(value)))),
+            Some(Err(error)) => {
+                self.set_disconnected(state, Reason::Err(error.into()), Source::Io);
+                Poll::Ready(ControlFlow::Continue(()))
+            }
+        }
+    }
+}
+
+impl<C, R, T, E> Stream for Tether<C, R>
+where
+    C: Io + Unpin,
+    C::Output: Stream<Item = Result<T, E>> + Unpin,
+    R: Resolver<C> + Unpin,
+    E: Into<std::io::Error>,
+{
+    type Item = Result<T, std::io::Error>;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        let me = self.get_mut();
+
+        connected!(me, poll_stream_inner, cx, None,)
+    }
+}
